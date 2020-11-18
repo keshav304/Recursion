d49d92f7e7beb4622bf378a4cedc7b32396b8c60
keshav jha <keshavjhaa2678@gmail.com>
2020-11-18 13:44:10 +0530

    Update sortAnArray.py

--- Sorting Array/sortAnArray.py
+++ Sorting Array/sortAnArray.py
@@ -1,4 +1,4 @@
-# Sorting An Array using Recursion
+# Sorting a Array using Recursion
 def insert(arr, temp):
     print("insert",arr, temp)
     # arr is sorted
@@ -7,8 +7,8 @@ def insert(arr, temp):
         arr.append(temp)
         return arr
     """
-    remove the last element and insert the temp element in smaller array
-    and then add the removed elements
+    remove the last element and insert the temporary element in the smaller array
+    and then add the removed elements after the temprorary element 
     """
 
     val = arr.pop()
@@ -18,7 +18,6 @@ def insert(arr, temp):
 
 
 def sort(arr):
-    # arr is unsorted
     # base condition
     if len(arr) == 1:
         return arr
@@ -27,11 +26,11 @@ def sort(arr):
     print("sort", arr, temp)
     # sort the rest of the array
     sort(arr)
-    # insert the removed element at correct position in the sorted array
+    # inserting the removed elements at correct position in the sorted array
     arr_sorted = insert(arr, temp)
     return arr_sorted
 
 
 arr_unsorted = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 print("Unsorted array", arr_unsorted)
-print("sorted array", sort(arr_unsorted))
+print("Sorted array", sort(arr_unsorted))
