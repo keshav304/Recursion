ceeee975de2b59fad62495d4671e717decdfa09d
keshav jha <keshavjhaa2678@gmail.com>
2020-11-20 15:13:49 +0530

    Update K-thSymbolinGrammar.py

--- Leetcode/K-thSymbolinGrammar.py
+++ Leetcode/K-thSymbolinGrammar.py
@@ -1,4 +1,8 @@
 # question: https://leetcode.com/problems/k-th-symbol-in-grammar/submissions/
+"""
+element of nth row is identical as elements of (n-1)th row upto the mid and after that they are opposite
+so if k<=mid print the kth element of (n-1)th row else its opposite value
+"""
 
 class Solution:
     def kthGrammar(self, N: int, K: int) -> int:
